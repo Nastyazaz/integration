19ebc19ab3a74646d2ffd9886ce5ab3ccacdb478
N <dynaeva.nastya96@gmail.com>
2024-10-05 14:41:56 +0300

    test coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,2 +1,29 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {10, 20, 30, 40, 50};
+        long expectedMax = 50;
+        long actualMax = service.findMax(incomes);
+        Assertions.assertEquals(expectedMax, actualMax);
+    }
 }
